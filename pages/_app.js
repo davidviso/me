3bc69b2eb3b16bdf24019f14404df3d481fafaf4
DavidViso <visotskydavid@gmail.com>
2022-01-12 00:45:31 -0500

    delete and begin anew

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/scss/globals.scss'
+
+import '../styles/scss/theme.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
